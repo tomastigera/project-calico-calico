6f6ba12e83725ff230dd7c2c41e36ed50ed1973b
Jiawei Huang <jiawei@tigera.io>
2025-05-22 22:16:00 -0700

    Fix libcalico-go client_test import

--- libcalico-go/lib/clientv3/client_test.go
+++ libcalico-go/lib/clientv3/client_test.go
@@ -3,7 +3,7 @@ package clientv3
 import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	v3 "github.com/projectcalico/api/pkg/apis/projectcalico/v3"
+	v3 "github.com/tigera/api/pkg/apis/projectcalico/v3"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
