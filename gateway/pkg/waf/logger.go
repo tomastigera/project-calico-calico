0317a7f359e28289e7839ed8ca584641f4f2d20c
Antony Guinard <antony@tigera.io>
2025-06-11 16:45:24 +0100

    Initial logger (without aggregation)

--- gateway/pkg/waf/logger.go
+++ gateway/pkg/waf/logger.go
@@ -1,11 +1,65 @@
 package waf
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/sirupsen/logrus"
 
+	"github.com/projectcalico/calico/felix/collector/file"
 	"github.com/projectcalico/calico/felix/proto"
+	v1 "github.com/projectcalico/calico/linseed/pkg/apis/v1"
 )
 
 func DebugLogger(wafEvent *proto.WAFEvent) {
 	logrus.Warnf("New WAF event! Need to do something about that! %v", wafEvent)
+}
+
+func NewFileLogger(directory string, filename string) (func(wafEvent *proto.WAFEvent), error) {
+
+	fileReporter := file.NewReporter(
+		directory,
+		filename,
+		100,
+		5,
+	)
+	err := fileReporter.Start()
+	if err != nil {
+		return nil, fmt.Errorf("failed to start file reporter: %w", err)
+	}
+
+	return func(wafEvent *proto.WAFEvent) {
+		wafLog := ConvertWAFEventToWAFLog(wafEvent)
+
+		err := fileReporter.Report([]*v1.WAFLog{wafLog})
+		if err != nil {
+			logrus.Errorf("Failed to report WAF log: %v", err)
+		}
+	}, nil
+}
+
+func ConvertWAFEventToWAFLog(r *proto.WAFEvent) *v1.WAFLog {
+	wafLog := &v1.WAFLog{
+		Timestamp: time.Unix(r.Timestamp.Seconds, int64(r.Timestamp.Nanos)).UTC(),
+		Method:    r.Request.Method,
+		Msg:       fmt.Sprintf("WAF detected %d violations [%s]", len(r.Rules), r.Action),
+		Path:      r.Request.Path,
+		Protocol:  r.Request.Version,
+		RequestId: r.TxId,
+		Source: &v1.WAFEndpoint{
+			IP:      r.SrcIp,
+			PortNum: r.SrcPort,
+		},
+	}
+	for _, rule := range r.Rules {
+		wafLog.Rules = append(wafLog.Rules, v1.WAFRuleHit{
+			Message:    rule.Rule.Message,
+			Disruptive: rule.Disruptive,
+			Id:         rule.Rule.Id,
+			Severity:   rule.Rule.Severity,
+			File:       rule.Rule.File,
+			Line:       rule.Rule.Line,
+		})
+	}
+	return wafLog
 }
---
+++ gateway/pkg/waf/logger_test.go
@@ -0,0 +1,60 @@
+package waf
+
+import (
+	"testing"
+	"time"
+
+	"github.com/projectcalico/calico/felix/proto"
+	v1 "github.com/projectcalico/calico/linseed/pkg/apis/v1"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+func TestConvertWAFEventToWAFLog(t *testing.T) {
+	now := time.Now().UTC()
+
+	event := &proto.WAFEvent{
+		TxId:    "tx001",
+		SrcIp:   "10.3.53.1",
+		SrcPort: 65500,
+		Rules: []*proto.WAFRuleHit{
+			{
+				Rule: &proto.WAFRule{
+					Id:       "1620",
+					Message:  "Fake rule",
+					Severity: "high",
+					File:     "/etc/m/test.conf",
+					Line:     "58800",
+				},
+				Disruptive: false,
+			},
+		},
+		Action: "pass",
+		Request: &proto.HTTPRequest{
+			Method:  "GET",
+			Path:    "/test",
+			Version: "HTTP/1.1",
+			Headers: map[string]string{
+				"Content-Type": "application/json",
+			},
+		},
+		Timestamp: timestamppb.New(now),
+	}
+	log := ConvertWAFEventToWAFLog(event)
+
+	require.Equal(t, now, log.Timestamp)
+	require.Equal(t, "/test", log.Path)
+	require.Equal(t, "GET", log.Method)
+	require.Equal(t, "tx001", log.RequestId)
+	require.Equal(t, "HTTP/1.1", log.Protocol)
+	require.Equal(t, []v1.WAFRuleHit{
+		{
+			Id:         "1620",
+			Message:    "Fake rule",
+			Severity:   "high",
+			File:       "/etc/m/test.conf",
+			Line:       "58800",
+			Disruptive: false,
+		},
+	}, log.Rules)
+}
