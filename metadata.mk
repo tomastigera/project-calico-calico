f9d4c00848353c82ed7c264a311486d11b429f76
Tian Feng <tian@tigera.io>
2025-07-18 08:56:09 -0700

    deps(CVE): Bump bundled k8s@v1.32.7

--- metadata.mk
+++ metadata.mk
@@ -11,7 +11,7 @@ ACK_GINKGO=ACK_GINKGO_DEPRECATIONS=1.16.5
 
 # Version of Kubernetes to use for tests, rancher/kubectl, and kubectl binary release in
 # compliance benchmarker, confd, and kube-controllers.
-K8S_VERSION=v1.32.6
+K8S_VERSION=v1.32.7
 
 # Version of various tools used in the build and tests.
 COREDNS_VERSION=1.5.2
