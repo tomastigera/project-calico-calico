f0424f2d03f9efbd4509571b00461d0e985f70be
Lorcan McVeigh <lorcan.mcveigh@gmail.com>
2025-07-25 10:18:17 +0100

    Add back grpc server for log collection

--- l7-collector/cmd/l7-collector/l7-collector.go
+++ l7-collector/cmd/l7-collector/l7-collector.go
@@ -5,9 +5,13 @@ package main
 import (
 	"context"
 	"flag"
+	"net"
+	"os"
 	"sync"
+	"syscall"
 
 	log "github.com/sirupsen/logrus"
+	"google.golang.org/grpc"
 
 	"github.com/projectcalico/calico/l7-collector/pkg/collector"
 	"github.com/projectcalico/calico/l7-collector/pkg/config"
@@ -18,6 +22,8 @@ import (
 
 func main() {
 	var ver bool
+	var grpcServer bool
+	flag.BoolVar(&grpcServer, "grpc", true, "Run gRPC server for log collection")
 	flag.BoolVar(&ver, "version", false, "Print version information")
 	flag.Parse()
 
@@ -45,17 +51,58 @@ func main() {
 
 	log.Infof("setting up Felixclient at %s", cfg.DialTarget)
 
+	if grpcServer {
+		// Start gRPC log collector
+		gRPCServerStart(cfg, reportCh)
+	}
+
 	// Start the log collector
-	CollectAndSend(context.Background(), felixClient, c)
+	CollectAndSend(context.Background(), felixClient, c, grpcServer)
 }
 
-func CollectAndSend(ctx context.Context, client felixclient.FelixClient, collector collector.EnvoyCollector) {
+func gRPCServerStart(cfg *config.Config, reportCh chan collector.EnvoyInfo) {
+	log.Info("Starting gRCP server...")
+	ctx := context.Background()
+	gs := grpc.NewServer()
+	grpcCollector := collector.NewEnvoyCollector(cfg, reportCh)
+	logServer := collector.NewLoggingServer(grpcCollector.ReceiveLogs)
+	logServer.RegisterAccessLogServiceServer(gs)
+	go grpcCollector.Start(ctx)
+
+	// Run gRPC server on separate goroutine so we catch any signals and clean up.
+	if cfg.ListenNetwork == "unix" {
+		_ = syscall.Unlink(cfg.ListenAddress)
+	}
+	lis, err := net.Listen(cfg.ListenNetwork, cfg.ListenAddress)
+	if err != nil {
+		log.Fatal("could not start listener: ", err)
+	}
+	if cfg.ListenNetwork == "unix" {
+		// anyone on system can connect.
+		if err := os.Chmod(cfg.ListenAddress, 0o777); err != nil {
+			log.Fatal("unable to set write permission on socket: ", err)
+		}
+	}
+	go func() {
+		if err := gs.Serve(lis); err != nil {
+			log.Errorf("failed to serve: %v", err)
+		}
+		defer lis.Close()
+	}()
+}
+
+func CollectAndSend(ctx context.Context, client felixclient.FelixClient, collector collector.EnvoyCollector, grpcServer bool) {
 	ctx, cancel := context.WithCancel(ctx)
 	wg := sync.WaitGroup{}
 
 	wg.Add(1)
 	go func() {
-		collector.ReadAccessLogs(ctx)
+		log.Info("Starting log collection...")
+		if grpcServer {
+			collector.ReadLogs(ctx)
+		} else {
+			collector.ReadAccessLogs(ctx)
+		}
 		cancel()
 		wg.Done()
 	}()
