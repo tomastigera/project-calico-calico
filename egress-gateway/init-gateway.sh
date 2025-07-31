86a5f75cd9f90748b748a1b3cd0c14ef7dbc783d
Casey Davenport <caseydavenport@users.noreply.github.com>
2025-07-31 16:59:59 -0700

    Nftables support for EGW init (#9439)
    * Nftables support for egw init
    
    * script fixes
    
    * script fixes

--- egress-gateway/init-gateway.sh
+++ egress-gateway/init-gateway.sh
@@ -5,6 +5,45 @@
 # runs as part of an init container, which runs in privileged mode.
 # This allows us to run the EGW pods as non-privileged.
 
+configure_iptables()
+{
+  MSS_CLAMP_VALUE=$1
+
+  # IPTABLES_BACKEND may be set to "legacy" or "nft".  If not defined
+  # try to auto-detect nft or legacy.
+  if [ "$IPTABLES_BACKEND" ]
+  then
+      echo "IPTABLES_BACKEND set to $IPTABLES_BACKEND"
+      iptables-${IPTABLES_BACKEND} -t nat -A POSTROUTING -j MASQUERADE
+      IPTABLES_BINARY="iptables-${IPTABLES_BACKEND}"
+  elif iptables-nft -t nat -A POSTROUTING -j MASQUERADE
+  then
+      IPTABLES_BINARY="iptables-nft"
+      echo "Successfully configured iptables with iptables-nft."
+  elif iptables-legacy -t nat -A POSTROUTING -j MASQUERADE
+  then
+      IPTABLES_BINARY="iptables-legacy"
+      echo "Successfully configured iptables with iptables-legacy."
+  else
+      echo "Failed to configure iptables (tried both nft and legacy)."
+      exit 1
+  fi
+
+  args="FORWARD -t mangle -o eth0 -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --set-mss $MSS_CLAMP_VALUE"
+  $IPTABLES_BINARY -C $args 2>/dev/null || $IPTABLES_BINARY -A $args
+}
+
+configure_nftables()
+{
+  MSS_CLAMP_VALUE=$1
+
+  nft -f - <<EOF
+create table egw
+add chain egw mangle-FORWARD { type filter hook forward priority mangle; }
+add rule ip egw mangle-FORWARD oifname "eth0" tcp flags & (syn|rst) == syn counter tcp option maxseg size set ${MSS_CLAMP_VALUE}
+EOF
+}
+
 set -e
 
 # Capture the usual signals and exit from the script
@@ -47,41 +86,29 @@ echo Egress VXLAN VNI: $EGRESS_VXLAN_VNI  VXLAN PORT: $EGRESS_VXLAN_PORT VXLAN M
 
 echo Configure iptable rules
 
-# IPTABLES_BACKEND may be set to "legacy" or "nft".  If not defined
-# try to auto-detect nft or legacy.
-
-if [ "$IPTABLES_BACKEND" ]
-then
-    echo "IPTABLES_BACKEND set to $IPTABLES_BACKEND"
-    iptables-${IPTABLES_BACKEND} -t nat -A POSTROUTING -j MASQUERADE
-    IPTABLES_BINARY="iptables-${IPTABLES_BACKEND}"
-elif iptables-nft -t nat -A POSTROUTING -j MASQUERADE
-then
-    IPTABLES_BINARY="iptables-nft"
-    echo "Successfully configured iptables with iptables-nft."
-elif iptables-legacy -t nat -A POSTROUTING -j MASQUERADE
-then
-    IPTABLES_BINARY="iptables-legacy"
-    echo "Successfully configured iptables with iptables-legacy."
-else
-    echo "Failed to configure iptables (tried both nft and legacy)."
-    exit 1
-fi
-
 echo Configure vxlan tunnel device
 ip link add vxlan0 type vxlan id $EGRESS_VXLAN_VNI dstport $EGRESS_VXLAN_PORT dev eth0 || printf " (and that's fine)"
 ip link set vxlan0 address $MAC
 ip link set vxlan0 up
 
+
+DATAPLANE=${DATAPLANE:-iptables}
+echo "Dataplane is ${DATAPLANE}"
+
 echo "Adding iptables MSS clamping rules on interface eth0"
+
 # Detect vxlan0 MTU
 VXLAN_MTU=`awk '{print $1}' /sys/class/net/vxlan0/mtu`
 
 # Calculate MSS_CLAMP_VALUE=VXLAN_MTU - 40 (IPv4 header len + TCP header len)
 MSS_CLAMP_VALUE="$(($VXLAN_MTU - 40))"
 echo "Detected vxlan0 MTU=$VXLAN_MTU. Clamping MSS value to $MSS_CLAMP_VALUE"
-args="FORWARD -t mangle -o eth0 -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --set-mss $MSS_CLAMP_VALUE"
-$IPTABLES_BINARY -C $args 2>/dev/null || $IPTABLES_BINARY -A $args
+if [ "$DATAPLANE" = "nftables" ]
+then
+  configure_nftables $MSS_CLAMP_VALUE
+else
+  configure_iptables $MSS_CLAMP_VALUE
+fi
 
 echo Configure network settings
 echo 1 > /proc/sys/net/ipv4/ip_forward
