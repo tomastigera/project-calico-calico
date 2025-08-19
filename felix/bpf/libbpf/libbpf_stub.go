5ceb729965263d5d6dbc2062610ef4f94c7964a1
Shaun Crampton <shaun@tigera.io>
2025-08-19 11:45:30 +0100

    Post-merge fix-up.

--- felix/bpf/libbpf/libbpf_stub.go
+++ felix/bpf/libbpf/libbpf_stub.go
@@ -247,7 +247,3 @@ func ProgName(id uint32) (string, error) {
 func (o *Obj) AttachTCX(secName, ifName string) (*Link, error) {
 	panic("LIBBPF syscall stub")
 }
-
-func OpenObjectWithLogBuffer(filename string, buf []byte) (*Obj, error) {
-	panic("LIBBPF syscall stub")
-}
