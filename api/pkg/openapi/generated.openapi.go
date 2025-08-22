projectcalico/calico

Set DSCP for workload's egress traffic leaving cluster (#10718)

api/pkg/openapi/generated.openapi.go
@@ -124,6 +124,7 @@ func GetOpenAPIDefinitions(ref common.ReferenceCallback) map[string]common.OpenA
 		"github.com/projectcalico/api/pkg/apis/projectcalico/v3.TierList":                           schema_pkg_apis_projectcalico_v3_TierList(ref),
 		"github.com/projectcalico/api/pkg/apis/projectcalico/v3.TierSpec":                           schema_pkg_apis_projectcalico_v3_TierSpec(ref),
 		"github.com/projectcalico/api/pkg/apis/projectcalico/v3.WorkloadEndpointControllerConfig":   schema_pkg_apis_projectcalico_v3_WorkloadEndpointControllerConfig(ref),
+		"github.com/projectcalico/api/pkg/lib/numorstring.DSCP":                                     schema_api_pkg_lib_numorstring_DSCP(ref),
 		"github.com/projectcalico/api/pkg/lib/numorstring.Port":                                     schema_api_pkg_lib_numorstring_Port(ref),
 		"github.com/projectcalico/api/pkg/lib/numorstring.Protocol":                                 schema_api_pkg_lib_numorstring_Protocol(ref),
 		"github.com/projectcalico/api/pkg/lib/numorstring.Uint8OrString":                            schema_api_pkg_lib_numorstring_Uint8OrString(ref),
@@ -6643,6 +6644,17 @@ func schema_pkg_apis_projectcalico_v3_WorkloadEndpointControllerConfig(ref commo
 		},
 		Dependencies: []string{
 			"k8s.io/apimachinery/pkg/apis/meta/v1.Duration"},
+	}
+}
+
+func schema_api_pkg_lib_numorstring_DSCP(ref common.ReferenceCallback) common.OpenAPIDefinition {
+	return common.OpenAPIDefinition{
+		Schema: spec.Schema{
+			SchemaProps: spec.SchemaProps{
+				Type:   numorstring.DSCP{}.OpenAPISchemaType(),
+				Format: numorstring.DSCP{}.OpenAPISchemaFormat(),
+			},
+		},
 	}
 }
 

libcalico-go/lib/apis/v3/generated.openapi.go
@@ -3253,9 +3253,16 @@ func schema_libcalico_go_lib_apis_v3_QoSControls(ref common.ReferenceCallback) c
 							Format:      "int64",
 						},
 					},
+					"dscp": {
+						SchemaProps: spec.SchemaProps{
+							Ref: ref("github.com/projectcalico/api/pkg/lib/numorstring.DSCP"),
+						},
+					},
 				},
 			},
 		},
+		Dependencies: []string{
+			"github.com/projectcalico/api/pkg/lib/numorstring.DSCP"},
 	}
 }
 

tomastigera/project-calico-calico

(cherry picked from commit c141d2dd175da5a6dd99518dae0a0c043d067e85)

api/pkg/openapi/generated.openapi.go
@@ -263,6 +263,7 @@ func GetOpenAPIDefinitions(ref common.ReferenceCallback) map[string]common.OpenA
 		"github.com/tigera/api/pkg/apis/projectcalico/v3.UISettingsList":                           schema_pkg_apis_projectcalico_v3_UISettingsList(ref),
 		"github.com/tigera/api/pkg/apis/projectcalico/v3.UISettingsSpec":                           schema_pkg_apis_projectcalico_v3_UISettingsSpec(ref),
 		"github.com/tigera/api/pkg/apis/projectcalico/v3.WorkloadEndpointControllerConfig":         schema_pkg_apis_projectcalico_v3_WorkloadEndpointControllerConfig(ref),
+		"github.com/tigera/api/pkg/lib/numorstring.DSCP":                                           schema_api_pkg_lib_numorstring_DSCP(ref),
 		"github.com/tigera/api/pkg/lib/numorstring.Port":                                           schema_api_pkg_lib_numorstring_Port(ref),
 		"github.com/tigera/api/pkg/lib/numorstring.Protocol":                                       schema_api_pkg_lib_numorstring_Protocol(ref),
 		"github.com/tigera/api/pkg/lib/numorstring.Uint8OrString":                                  schema_api_pkg_lib_numorstring_Uint8OrString(ref),
@@ -14146,6 +14147,17 @@ func schema_pkg_apis_projectcalico_v3_WorkloadEndpointControllerConfig(ref commo
 		},
 		Dependencies: []string{
 			"k8s.io/apimachinery/pkg/apis/meta/v1.Duration"},
+	}
+}
+
+func schema_api_pkg_lib_numorstring_DSCP(ref common.ReferenceCallback) common.OpenAPIDefinition {
+	return common.OpenAPIDefinition{
+		Schema: spec.Schema{
+			SchemaProps: spec.SchemaProps{
+				Type:   numorstring.DSCP{}.OpenAPISchemaType(),
+				Format: numorstring.DSCP{}.OpenAPISchemaFormat(),
+			},
+		},
 	}
 }
 

libcalico-go/lib/apis/v3/generated.openapi.go
@@ -3367,9 +3367,16 @@ func schema_libcalico_go_lib_apis_v3_QoSControls(ref common.ReferenceCallback) c
 							Format:      "int64",
 						},
 					},
+					"dscp": {
+						SchemaProps: spec.SchemaProps{
+							Ref: ref("github.com/tigera/api/pkg/lib/numorstring.DSCP"),
+						},
+					},
 				},
 			},
 		},
+		Dependencies: []string{
+			"github.com/tigera/api/pkg/lib/numorstring.DSCP"},
 	}
 }
 