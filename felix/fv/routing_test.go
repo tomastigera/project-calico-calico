e9477ff8dffeeb4e8d256e4a2bc0e60d6c0b9c79
Mazdak Nasab <mazdak.nasab@gmail.com>
2025-08-29 11:34:20 -0700

    update

--- felix/fv/routing_test.go
+++ felix/fv/routing_test.go
@@ -585,7 +585,7 @@ var _ = infrastructure.DatastoreDescribe("_BPF-SAFE_ cluster routing using Felix
 				})
 			})
 
-			Context("pepper after removing BGP address from third node", func() {
+			Context("after removing BGP address from third node", func() {
 				// Simulate having a host send IPIP traffic from an unknown source, should get blocked.
 				BeforeEach(func() {
 					if ipipMode == api.IPIPModeNever {
