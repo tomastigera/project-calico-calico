83ba1db3085b384a492b82e0ac24f42d1e314fd7
Lorcan McVeigh <lorcan.mcveigh@gmail.com>
2025-09-17 10:35:00 +0100

    Remove unused reporting logic

--- intrusion-detection-controller/pkg/globalalert/reporting/reporting.go
+++ intrusion-detection-controller/pkg/globalalert/reporting/reporting.go
@@ -2,16 +2,13 @@ package reporting
 
 import (
 	"context"
-	"fmt"
 	"time"
 
 	log "github.com/sirupsen/logrus"
 	v3 "github.com/tigera/api/pkg/apis/projectcalico/v3"
 	calicoclient "github.com/tigera/api/pkg/client/clientset_generated/clientset"
-	batchv1 "k8s.io/api/batch/v1"
-	k8serrors "k8s.io/apimachinery/pkg/api/errors"
+
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/util/retry"
 )
 
@@ -40,48 +37,6 @@ func updateGlobalAlertStatus(globalAlert *v3.GlobalAlert, clusterName string, ca
 		return err
 	}
 	return nil
-}
-
-// WatchAndReportJobStatus uses the jobwatcher to retrieve events for a max duration specified by waitTime. Returns a
-// GlobalAlertStatus based on the event received, error GlobalAlertStatus is returned if no events are recevied before
-// waitTime expires
-func WatchAndReportJobStatus(jobWatcher watch.Interface, jobName string, waitTime time.Duration) (v3.GlobalAlertStatus, error) {
-	var done <-chan time.Time // signal for when timer us up
-
-	for {
-		if done == nil {
-			done = time.After(waitTime)
-		}
-
-		select {
-		case <-done:
-			resultChanError := fmt.Errorf("did not receive any events after timeout from Job %s", jobName)
-			return GetGlobalAlertErrorStatus(resultChanError), resultChanError
-		case event, open := <-jobWatcher.ResultChan():
-			if !open {
-				continue
-			}
-
-			job, ok := event.Object.(*batchv1.Job)
-
-			if !ok || job.Name != jobName {
-				log.Error("unexpected type")
-				continue
-			}
-
-			switch event.Type {
-			case watch.Error:
-				trainingError := k8serrors.FromObject(job)
-				return GetGlobalAlertErrorStatus(trainingError), trainingError
-			case watch.Deleted: // deleted event is sent on successful shutdown
-				return GetGlobalAlertSuccessStatus(), nil
-			case watch.Added:
-			case watch.Modified:
-			default:
-				continue
-			}
-		}
-	}
 }
 
 // GetGlobalAlertErrorStatus creates a
