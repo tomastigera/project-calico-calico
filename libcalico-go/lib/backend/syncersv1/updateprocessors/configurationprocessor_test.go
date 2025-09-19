38754e47861cf79b329abd6d92b885714ac4c66f
Tomas Hruby <tomas@tigera.io>
2025-09-19 11:39:44 -0700

    update numBaseFelixConfigs

--- libcalico-go/lib/backend/syncersv1/updateprocessors/configurationprocessor_test.go
+++ libcalico-go/lib/backend/syncersv1/updateprocessors/configurationprocessor_test.go
@@ -45,7 +45,7 @@ const (
 
 const (
 	// numBaseFelixConfigs should match the OS value.
-	numBaseFelixConfigs = 167
+	numBaseFelixConfigs = 168
 
 	// Add private-only fields to this count.
 	numPrivateOnlyFelixConfigs = 115
