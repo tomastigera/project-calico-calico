e00b80b12517b5a87639fd1409ab9e377e86f3cc
Casey Davenport <davenport.cas@gmail.com>
2025-09-25 14:08:12 -0700

    Fix wrong api import

--- libcalico-go/lib/ipam/namespace_selector_test.go
+++ libcalico-go/lib/ipam/namespace_selector_test.go
@@ -17,7 +17,7 @@ package ipam
 import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	v3 "github.com/projectcalico/api/pkg/apis/projectcalico/v3"
+	v3 "github.com/tigera/api/pkg/apis/projectcalico/v3"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
