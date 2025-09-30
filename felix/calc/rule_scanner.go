projectcalico/calico

iptables: Force-program chains for AssumeNeededOnEveryNode policies
- Pass the hints down to the dataplane.
- For the for forward table only (since it's the mainline and it's
  generally hard to tell in advance which table is needed); set a
  flag on marked policies.
- In iptables layer, incref policies with the flag so that they get
  programmed.

felix/calc/rule_scanner.go
@@ -18,6 +18,7 @@ import (
 	"fmt"
 	"strings"
 
+	apiv3 "github.com/projectcalico/api/pkg/apis/projectcalico/v3"
 	"github.com/projectcalico/api/pkg/lib/numorstring"
 	log "github.com/sirupsen/logrus"
 
@@ -158,12 +159,12 @@ func NewRuleScanner() *RuleScanner {
 }
 
 func (rs *RuleScanner) OnProfileActive(key model.ProfileRulesKey, profile *model.ProfileRules) {
-	parsedRules := rs.updateRules(key, profile.InboundRules, profile.OutboundRules, false, false, "", "")
+	parsedRules := rs.updateRules(key, profile.InboundRules, profile.OutboundRules, false, false, "", "", nil)
 	rs.RulesUpdateCallbacks.OnProfileActive(key, parsedRules)
 }
 
 func (rs *RuleScanner) OnProfileInactive(key model.ProfileRulesKey) {
-	rs.updateRules(key, nil, nil, false, false, "", "")
+	rs.updateRules(key, nil, nil, false, false, "", "", nil)
 	rs.RulesUpdateCallbacks.OnProfileInactive(key)
 }
 
@@ -176,12 +177,13 @@ func (rs *RuleScanner) OnPolicyActive(key model.PolicyKey, policy *model.Policy)
 		policy.PreDNAT,
 		policy.Namespace,
 		selector.Normalise(policy.Selector),
+		policy.PerformanceHints,
 	)
 	rs.RulesUpdateCallbacks.OnPolicyActive(key, parsedRules)
 }
 
 func (rs *RuleScanner) OnPolicyInactive(key model.PolicyKey) {
-	rs.updateRules(key, nil, nil, false, false, "", "")
+	rs.updateRules(key, nil, nil, false, false, "", "", nil)
 	rs.RulesUpdateCallbacks.OnPolicyInactive(key)
 }
 
@@ -191,6 +193,7 @@ func (rs *RuleScanner) updateRules(
 	untracked, preDNAT bool,
 	origNamespace string,
 	origSelector string,
+	perfHints []apiv3.PolicyPerformanceHint,
 ) (parsedRules *ParsedRules) {
 	log.Debugf("Scanning rules (%v in, %v out) for key %v",
 		len(inbound), len(outbound), key)
@@ -225,6 +228,7 @@ func (rs *RuleScanner) updateRules(
 		Untracked:        untracked,
 		PreDNAT:          preDNAT,
 		OriginalSelector: origSelector,
+		PerformanceHints: perfHints,
 	}
 
 	// Figure out which IP sets are new.
@@ -297,6 +301,8 @@ type ParsedRules struct {
 	PreDNAT bool
 
 	OriginalSelector string
+
+	PerformanceHints []apiv3.PolicyPerformanceHint
 }
 
 // ParsedRule is like a backend.model.Rule, except the selector matches and named ports are

felix/proto/felixbackend.pb.go
@@ -2045,12 +2045,13 @@ type Policy struct {
 	state protoimpl.MessageState `protogen:"open.v1"`
 	// If the Policy represents a NetworkPolicy, this contains the namespace that the policy came
 	// from.  Otherwise, empty.
-	Namespace        string  `protobuf:"bytes,5,opt,name=namespace,proto3" json:"namespace,omitempty"`
-	InboundRules     []*Rule `protobuf:"bytes,1,rep,name=inbound_rules,json=inboundRules,proto3" json:"inbound_rules,omitempty"`
-	OutboundRules    []*Rule `protobuf:"bytes,2,rep,name=outbound_rules,json=outboundRules,proto3" json:"outbound_rules,omitempty"`
-	Untracked        bool    `protobuf:"varint,3,opt,name=untracked,proto3" json:"untracked,omitempty"`
-	PreDnat          bool    `protobuf:"varint,4,opt,name=pre_dnat,json=preDnat,proto3" json:"pre_dnat,omitempty"`
-	OriginalSelector string  `protobuf:"bytes,6,opt,name=original_selector,json=originalSelector,proto3" json:"original_selector,omitempty"`
+	Namespace        string   `protobuf:"bytes,5,opt,name=namespace,proto3" json:"namespace,omitempty"`
+	InboundRules     []*Rule  `protobuf:"bytes,1,rep,name=inbound_rules,json=inboundRules,proto3" json:"inbound_rules,omitempty"`
+	OutboundRules    []*Rule  `protobuf:"bytes,2,rep,name=outbound_rules,json=outboundRules,proto3" json:"outbound_rules,omitempty"`
+	Untracked        bool     `protobuf:"varint,3,opt,name=untracked,proto3" json:"untracked,omitempty"`
+	PreDnat          bool     `protobuf:"varint,4,opt,name=pre_dnat,json=preDnat,proto3" json:"pre_dnat,omitempty"`
+	OriginalSelector string   `protobuf:"bytes,6,opt,name=original_selector,json=originalSelector,proto3" json:"original_selector,omitempty"`
+	PerfHints        []string `protobuf:"bytes,7,rep,name=perf_hints,json=perfHints,proto3" json:"perf_hints,omitempty"`
 	unknownFields    protoimpl.UnknownFields
 	sizeCache        protoimpl.SizeCache
 }
@@ -2125,6 +2126,13 @@ func (x *Policy) GetOriginalSelector() string {
 		return x.OriginalSelector
 	}
 	return ""
+}
+
+func (x *Policy) GetPerfHints() []string {
+	if x != nil {
+		return x.PerfHints
+	}
+	return nil
 }
 
 type Rule struct {
@@ -6501,14 +6509,16 @@ const file_felixbackend_proto_rawDesc = "" +
 	"\x02id\x18\x01 \x01(\v2\x0f.felix.PolicyIDR\x02id\"2\n" +
 	"\bPolicyID\x12\x12\n" +
 	"\x04tier\x18\x01 \x01(\tR\x04tier\x12\x12\n" +
-	"\x04name\x18\x02 \x01(\tR\x04name\"\xf2\x01\n" +
+	"\x04name\x18\x02 \x01(\tR\x04name\"\x91\x02\n" +
 	"\x06Policy\x12\x1c\n" +
 	"\tnamespace\x18\x05 \x01(\tR\tnamespace\x120\n" +
 	"\rinbound_rules\x18\x01 \x03(\v2\v.felix.RuleR\finboundRules\x122\n" +
 	"\x0eoutbound_rules\x18\x02 \x03(\v2\v.felix.RuleR\routboundRules\x12\x1c\n" +
 	"\tuntracked\x18\x03 \x01(\bR\tuntracked\x12\x19\n" +
 	"\bpre_dnat\x18\x04 \x01(\bR\apreDnat\x12+\n" +
-	"\x11original_selector\x18\x06 \x01(\tR\x10originalSelector\"\xaa\x10\n" +
+	"\x11original_selector\x18\x06 \x01(\tR\x10originalSelector\x12\x1d\n" +
+	"\n" +
+	"perf_hints\x18\a \x03(\tR\tperfHints\"\xaa\x10\n" +
 	"\x04Rule\x12\x16\n" +
 	"\x06action\x18\x01 \x01(\tR\x06action\x12/\n" +
 	"\n" +

tomastigera/project-calico-calico

felix/calc/rule_scanner.go
@@ -19,6 +19,7 @@ import (
 	"strings"
 
 	log "github.com/sirupsen/logrus"
+	apiv3 "github.com/tigera/api/pkg/apis/projectcalico/v3"
 	"github.com/tigera/api/pkg/lib/numorstring"
 
 	"github.com/projectcalico/calico/felix/labelindex"
@@ -189,12 +190,12 @@ func NewRuleScanner() *RuleScanner {
 }
 
 func (rs *RuleScanner) OnProfileActive(key model.ProfileRulesKey, profile *model.ProfileRules) {
-	parsedRules := rs.updateRules(key, profile.InboundRules, profile.OutboundRules, false, false, "", "")
+	parsedRules := rs.updateRules(key, profile.InboundRules, profile.OutboundRules, false, false, "", "", nil)
 	rs.RulesUpdateCallbacks.OnProfileActive(key, parsedRules)
 }
 
 func (rs *RuleScanner) OnProfileInactive(key model.ProfileRulesKey) {
-	rs.updateRules(key, nil, nil, false, false, "", "")
+	rs.updateRules(key, nil, nil, false, false, "", "", nil)
 	rs.RulesUpdateCallbacks.OnProfileInactive(key)
 }
 
@@ -207,12 +208,13 @@ func (rs *RuleScanner) OnPolicyActive(key model.PolicyKey, policy *model.Policy)
 		policy.PreDNAT,
 		policy.Namespace,
 		selector.Normalise(policy.Selector),
+		policy.PerformanceHints,
 	)
 	rs.RulesUpdateCallbacks.OnPolicyActive(key, parsedRules)
 }
 
 func (rs *RuleScanner) OnPolicyInactive(key model.PolicyKey) {
-	rs.updateRules(key, nil, nil, false, false, "", "")
+	rs.updateRules(key, nil, nil, false, false, "", "", nil)
 	rs.RulesUpdateCallbacks.OnPolicyInactive(key)
 }
 
@@ -222,6 +224,7 @@ func (rs *RuleScanner) updateRules(
 	untracked, preDNAT bool,
 	origNamespace string,
 	origSelector string,
+	perfHints []apiv3.PolicyPerformanceHint,
 ) (parsedRules *ParsedRules) {
 	log.Debugf("Scanning rules (%v in, %v out) for key %v",
 		len(inbound), len(outbound), key)
@@ -263,6 +266,7 @@ func (rs *RuleScanner) updateRules(
 		Untracked:        untracked,
 		PreDNAT:          preDNAT,
 		OriginalSelector: origSelector,
+		PerformanceHints: perfHints,
 	}
 
 	// Figure out which IP sets are new.
@@ -348,6 +352,8 @@ type ParsedRules struct {
 	PreDNAT bool
 
 	OriginalSelector string
+
+	PerformanceHints []apiv3.PolicyPerformanceHint
 }
 
 // ParsedRule is like a backend.model.Rule, except the selector matches and named ports are

felix/proto/felixbackend.pb.go
@@ -2382,12 +2382,13 @@ type Policy struct {
 	state protoimpl.MessageState `protogen:"open.v1"`
 	// If the Policy represents a NetworkPolicy, this contains the namespace that the policy came
 	// from.  Otherwise, empty.
-	Namespace        string  `protobuf:"bytes,5,opt,name=namespace,proto3" json:"namespace,omitempty"`
-	InboundRules     []*Rule `protobuf:"bytes,1,rep,name=inbound_rules,json=inboundRules,proto3" json:"inbound_rules,omitempty"`
-	OutboundRules    []*Rule `protobuf:"bytes,2,rep,name=outbound_rules,json=outboundRules,proto3" json:"outbound_rules,omitempty"`
-	Untracked        bool    `protobuf:"varint,3,opt,name=untracked,proto3" json:"untracked,omitempty"`
-	PreDnat          bool    `protobuf:"varint,4,opt,name=pre_dnat,json=preDnat,proto3" json:"pre_dnat,omitempty"`
-	OriginalSelector string  `protobuf:"bytes,6,opt,name=original_selector,json=originalSelector,proto3" json:"original_selector,omitempty"`
+	Namespace        string   `protobuf:"bytes,5,opt,name=namespace,proto3" json:"namespace,omitempty"`
+	InboundRules     []*Rule  `protobuf:"bytes,1,rep,name=inbound_rules,json=inboundRules,proto3" json:"inbound_rules,omitempty"`
+	OutboundRules    []*Rule  `protobuf:"bytes,2,rep,name=outbound_rules,json=outboundRules,proto3" json:"outbound_rules,omitempty"`
+	Untracked        bool     `protobuf:"varint,3,opt,name=untracked,proto3" json:"untracked,omitempty"`
+	PreDnat          bool     `protobuf:"varint,4,opt,name=pre_dnat,json=preDnat,proto3" json:"pre_dnat,omitempty"`
+	OriginalSelector string   `protobuf:"bytes,6,opt,name=original_selector,json=originalSelector,proto3" json:"original_selector,omitempty"`
+	PerfHints        []string `protobuf:"bytes,7,rep,name=perf_hints,json=perfHints,proto3" json:"perf_hints,omitempty"`
 	unknownFields    protoimpl.UnknownFields
 	sizeCache        protoimpl.SizeCache
 }
@@ -2462,6 +2463,13 @@ func (x *Policy) GetOriginalSelector() string {
 		return x.OriginalSelector
 	}
 	return ""
+}
+
+func (x *Policy) GetPerfHints() []string {
+	if x != nil {
+		return x.PerfHints
+	}
+	return nil
 }
 
 type Rule struct {
@@ -8577,14 +8585,16 @@ const file_felixbackend_proto_rawDesc = "" +
 	"\x02id\x18\x01 \x01(\v2\x0f.felix.PolicyIDR\x02id\"2\n" +
 	"\bPolicyID\x12\x12\n" +
 	"\x04tier\x18\x01 \x01(\tR\x04tier\x12\x12\n" +
-	"\x04name\x18\x02 \x01(\tR\x04name\"\xf2\x01\n" +
+	"\x04name\x18\x02 \x01(\tR\x04name\"\x91\x02\n" +
 	"\x06Policy\x12\x1c\n" +
 	"\tnamespace\x18\x05 \x01(\tR\tnamespace\x120\n" +
 	"\rinbound_rules\x18\x01 \x03(\v2\v.felix.RuleR\finboundRules\x122\n" +
 	"\x0eoutbound_rules\x18\x02 \x03(\v2\v.felix.RuleR\routboundRules\x12\x1c\n" +
 	"\tuntracked\x18\x03 \x01(\bR\tuntracked\x12\x19\n" +
 	"\bpre_dnat\x18\x04 \x01(\bR\apreDnat\x12+\n" +
-	"\x11original_selector\x18\x06 \x01(\tR\x10originalSelector\"\xe8\x10\n" +
+	"\x11original_selector\x18\x06 \x01(\tR\x10originalSelector\x12\x1d\n" +
+	"\n" +
+	"perf_hints\x18\a \x03(\tR\tperfHints\"\xe8\x10\n" +
 	"\x04Rule\x12\x16\n" +
 	"\x06action\x18\x01 \x01(\tR\x06action\x12/\n" +
 	"\n" +