projectcalico/calico

use a single ProxierHealthServer for dualstack
We run two instance of kube-proxy, but there is only a single probe for
both of them. So we need to share the healthz server like the upstream
kube-proxy does.

felix/bpf/proxy/health_check_nodeport_test.go
@@ -15,6 +15,7 @@
 package proxy_test
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"net/http"
@@ -27,6 +28,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
 	"k8s.io/client-go/kubernetes/fake"
+	"k8s.io/kubernetes/pkg/proxy/healthcheck"
 
 	"github.com/projectcalico/calico/felix/bpf/proxy"
 )
@@ -38,12 +40,18 @@ var _ = Describe("BPF Proxy healthCheckNodeport", func() {
 	testNodeName := "testnode"
 	testNodeNameOther := "anothertestnode"
 
+	healthzServer := healthcheck.NewProxierHealthServer(":10256", 200*time.Millisecond)
+	go func() {
+		_ = healthzServer.Run(context.Background())
+	}()
+
 	BeforeEach(func() {
 		By("creating proxy with fake client and mock syncer", func() {
 			var err error
 
 			p, err = proxy.New(k8s, &mockDummySyncer{},
-				testNodeName, proxy.WithMinSyncPeriod(200*time.Millisecond))
+				testNodeName, proxy.WithMinSyncPeriod(200*time.Millisecond),
+				proxy.WithHealthzServer(healthzServer))
 			Expect(err).NotTo(HaveOccurred())
 		})
 	})

felix/bpf/proxy/options.go
@@ -19,6 +19,7 @@ import (
 	"time"
 
 	log "github.com/sirupsen/logrus"
+	"k8s.io/kubernetes/pkg/proxy/healthcheck"
 
 	"github.com/projectcalico/calico/felix/ip"
 )
@@ -109,6 +110,13 @@ func WithExcludedCIDRs(cidrs []string) Option {
 			kp.excludedCIDRs.Update(cidr, &excludeCIDRsMatch)
 		}
 
+		return nil
+	})
+}
+
+func WithHealthzServer(hs *healthcheck.ProxierHealthServer) Option {
+	return makeOption(func(p *proxy) error {
+		p.healthzServer = hs
 		return nil
 	})
 }

tomastigera/project-calico-calico

felix/bpf/proxy/health_check_nodeport_test.go
@@ -15,6 +15,7 @@
 package proxy_test
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"net/http"
@@ -27,6 +28,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
 	"k8s.io/client-go/kubernetes/fake"
+	"k8s.io/kubernetes/pkg/proxy/healthcheck"
 	"k8s.io/utils/ptr"
 
 	"github.com/projectcalico/calico/felix/bpf/proxy"
@@ -39,12 +41,18 @@ var _ = Describe("BPF Proxy healthCheckNodeport", func() {
 	testNodeName := "testnode"
 	testNodeNameOther := "anothertestnode"
 
+	healthzServer := healthcheck.NewProxyHealthServer(":10256", 200*time.Millisecond)
+	go func() {
+		_ = healthzServer.Run(context.Background())
+	}()
+
 	BeforeEach(func() {
 		By("creating proxy with fake client and mock syncer", func() {
 			var err error
 
 			p, err = proxy.New(k8s, &mockDummySyncer{},
-				testNodeName, proxy.WithMinSyncPeriod(200*time.Millisecond))
+				testNodeName, proxy.WithMinSyncPeriod(200*time.Millisecond),
+				proxy.WithHealthzServer(healthzServer))
 			Expect(err).NotTo(HaveOccurred())
 		})
 	})

felix/bpf/proxy/options.go
@@ -19,6 +19,7 @@ import (
 	"time"
 
 	log "github.com/sirupsen/logrus"
+	"k8s.io/kubernetes/pkg/proxy/healthcheck"
 
 	"github.com/projectcalico/calico/felix/ip"
 )
@@ -109,6 +110,13 @@ func WithExcludedCIDRs(cidrs []string) Option {
 			kp.excludedCIDRs.Update(cidr, &excludeCIDRsMatch)
 		}
 
+		return nil
+	})
+}
+
+func WithHealthzServer(hs *healthcheck.ProxyHealthServer) Option {
+	return makeOption(func(p *proxy) error {
+		p.healthzServer = hs
 		return nil
 	})
 }