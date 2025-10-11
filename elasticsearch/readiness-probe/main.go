38618cb4561591607fb3100a5034219619b396db
Jiawei Huang <jiawei@tigera.io>
2025-10-10 19:09:45 -0700

    Fix elasticsearch probe static checks

--- elasticsearch/readiness-probe/main.go
+++ elasticsearch/readiness-probe/main.go
@@ -87,7 +87,7 @@ func main() {
 	if err != nil {
 		fail("readiness probe failed", err)
 	}
-	defer resp.Body.Close()
+	defer func() { _ = resp.Body.Close() }()
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
