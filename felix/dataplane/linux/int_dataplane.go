e8edf175307c5022cf1b7fabd2d1600d21a98df5
sridhar <sridhar@tigera.io>
2025-10-21 14:36:22 -0700

    Enable fib for ipip, wireguard

--- felix/dataplane/linux/int_dataplane.go
+++ felix/dataplane/linux/int_dataplane.go
@@ -1324,12 +1324,12 @@ func NewIntDataplaneDriver(config Config, stopChan chan *sync.WaitGroup) *Intern
 
 		// Forwarding into an IPIP tunnel fails silently because IPIP tunnels are L3 devices and support for
 		// L3 devices in BPF is not available yet.  Disable the FIB lookup in that case.
-		fibLookupEnabled := !config.RulesConfig.IPIPEnabled
+		//fibLookupEnabled := !config.RulesConfig.IPIPEnabled
 		bpfEndpointManager, err = NewBPFEndpointManager(
 			nil,
 			&config,
 			bpfMaps,
-			fibLookupEnabled,
+			true,
 			workloadIfaceRegex,
 			ipSetIDAllocatorV4,
 			ipSetIDAllocatorV6,
