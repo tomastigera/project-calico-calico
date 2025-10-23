projectcalico/calico

eBPF: Rename ClusterExternal flag to SetDSCP (#11134)

felix/bpf/conntrack/v4/map.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2022 Tigera, Inc. All rights reserved.
+// Copyright (c) 2022-2025 Tigera, Inc. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -208,22 +208,22 @@ const (
 	TypeNATForward
 	TypeNATReverse
 
-	FlagNATOut          uint32 = (1 << 0)
-	FlagNATFwdDsr       uint32 = (1 << 1)
-	FlagNATNPFwd        uint32 = (1 << 2)
-	FlagSkipFIB         uint32 = (1 << 3)
-	FlagReserved4       uint32 = (1 << 4)
-	FlagReserved5       uint32 = (1 << 5)
-	FlagExtLocal        uint32 = (1 << 6)
-	FlagViaNATIf        uint32 = (1 << 7)
-	FlagSrcDstBA        uint32 = (1 << 8)
-	FlagHostPSNAT       uint32 = (1 << 9)
-	FlagSvcSelf         uint32 = (1 << 10)
-	FlagNPLoop          uint32 = (1 << 11)
-	FlagNPRemote        uint32 = (1 << 12)
-	FlagNoDSR           uint32 = (1 << 13)
-	FlagNoRedirPeer     uint32 = (1 << 14)
-	FlagClusterExternal uint32 = (1 << 15)
+	FlagNATOut      uint32 = (1 << 0)
+	FlagNATFwdDsr   uint32 = (1 << 1)
+	FlagNATNPFwd    uint32 = (1 << 2)
+	FlagSkipFIB     uint32 = (1 << 3)
+	FlagReserved4   uint32 = (1 << 4)
+	FlagReserved5   uint32 = (1 << 5)
+	FlagExtLocal    uint32 = (1 << 6)
+	FlagViaNATIf    uint32 = (1 << 7)
+	FlagSrcDstBA    uint32 = (1 << 8)
+	FlagHostPSNAT   uint32 = (1 << 9)
+	FlagSvcSelf     uint32 = (1 << 10)
+	FlagNPLoop      uint32 = (1 << 11)
+	FlagNPRemote    uint32 = (1 << 12)
+	FlagNoDSR       uint32 = (1 << 13)
+	FlagNoRedirPeer uint32 = (1 << 14)
+	FlagSetDSCP     uint32 = (1 << 15)
 )
 
 func (e Value) ReverseNATKey() KeyInterface {
@@ -509,6 +509,9 @@ func (e Value) String() string {
 		}
 		if flags&FlagNoRedirPeer != 0 {
 			flagsStr += " no-redir-peer"
+		}
+		if flags&FlagSetDSCP != 0 {
+			flagsStr += " dscp"
 		}
 	}
 

tomastigera/project-calico-calico

felix/bpf/conntrack/v4/map.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2022 Tigera, Inc. All rights reserved.
+// Copyright (c) 2022-2025 Tigera, Inc. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -206,22 +206,22 @@ const (
 	TypeNATForward
 	TypeNATReverse
 
-	FlagNATOut          uint16 = (1 << 0)
-	FlagNATFwdDsr       uint16 = (1 << 1)
-	FlagNATNPFwd        uint16 = (1 << 2)
-	FlagSkipFIB         uint16 = (1 << 3)
-	FlagReserved4       uint16 = (1 << 4)
-	FlagReserved5       uint16 = (1 << 5)
-	FlagExtLocal        uint16 = (1 << 6)
-	FlagViaNATIf        uint16 = (1 << 7)
-	FlagSrcDstBA        uint16 = (1 << 8)
-	FlagHostPSNAT       uint16 = (1 << 9)
-	FlagSvcSelf         uint16 = (1 << 10)
-	FlagNPLoop          uint16 = (1 << 11)
-	FlagNPRemote        uint16 = (1 << 12)
-	FlagNoDSR           uint16 = (1 << 13)
-	FlagNoRedirPeer     uint16 = (1 << 14)
-	FlagClusterExternal uint16 = (1 << 15)
+	FlagNATOut      uint16 = (1 << 0)
+	FlagNATFwdDsr   uint16 = (1 << 1)
+	FlagNATNPFwd    uint16 = (1 << 2)
+	FlagSkipFIB     uint16 = (1 << 3)
+	FlagReserved4   uint16 = (1 << 4)
+	FlagReserved5   uint16 = (1 << 5)
+	FlagExtLocal    uint16 = (1 << 6)
+	FlagViaNATIf    uint16 = (1 << 7)
+	FlagSrcDstBA    uint16 = (1 << 8)
+	FlagHostPSNAT   uint16 = (1 << 9)
+	FlagSvcSelf     uint16 = (1 << 10)
+	FlagNPLoop      uint16 = (1 << 11)
+	FlagNPRemote    uint16 = (1 << 12)
+	FlagNoDSR       uint16 = (1 << 13)
+	FlagNoRedirPeer uint16 = (1 << 14)
+	FlagSetDSCP     uint16 = (1 << 15)
 )
 
 func (e Value) ReverseNATKey() KeyInterface {
@@ -505,6 +505,9 @@ func (e Value) String() string {
 		}
 		if flags&FlagNoRedirPeer != 0 {
 			flagsStr += " no-redir-peer"
+		}
+		if flags&FlagSetDSCP != 0 {
+			flagsStr += " dscp"
 		}
 	}
 