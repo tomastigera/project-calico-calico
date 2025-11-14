4f7d2a30b72486faf21bd6bc86b87190f9d927d0
Tomas Hruby <49207409+tomastigera@users.noreply.github.com>
2025-11-14 03:32:52 -0800

    [master] Run BPF UTs on Ubuntu 25.04; test runs with bpf_jit_hardening=2 (#10033)
    Pick of
    
    - https://github.com/projectcalico/calico/pull/10783
    - https://github.com/projectcalico/calico/pull/11123
    - https://github.com/projectcalico/calico/pull/11274
    - https://github.com/projectcalico/calico/pull/11274

--- .semaphore/vms/vm-tests-common.sh
+++
@@ -1,8 +0,0 @@
-# Common definitions for the tests that are run on cloud
-# VMs rather than Semaphore VMs
-
-batches=(k8s-test enet egw-none egw-ipip egw-vxlan)
-
-# These batches are broken due to upstream changes to kind/k8s, fixing them
-# is tracked by CORE-11685.
-disabled_batches=(dual-tor dual-tor-legacy)
---
+++ node/.semaphore/batches.sh
@@ -0,0 +1,41 @@
+# Common definitions for the tests that are run on cloud
+# VMs rather than Semaphore VMs
+
+batches=(k8s-test dual-tor dual-tor-legacy enet egw-none egw-ipip egw-vxlan)
+
+run_batch() {
+  local remote_exec="$1"
+  local batch="$2"
+  local vm_name="$3"
+  local log_file="$4"
+  local pid
+
+  case $batch in
+    k8s-test)
+      cmd="make --directory=calico-private/${REPO_NAME} k8s-test"
+      ;;
+    dual-tor)
+      cmd="make --directory=calico-private/${REPO_NAME} dual-tor-test"
+      ;;
+    dual-tor-legacy)
+      cmd="make --directory=calico-private/${REPO_NAME} dual-tor-test-legacy"
+      ;;
+    enet)
+      cmd="make --directory=calico-private/${REPO_NAME} external-network-test"
+      ;;
+    egw-none)
+      cmd="K8ST_TO_RUN=\"-A egress_ip_no_overlay\" K8ST_REPORT_FILENAME=node-k8s-egress-ip-no-overlay.xml make --directory=calico-private/${REPO_NAME} k8s-test"
+      ;;
+    egw-ipip)
+      cmd="K8ST_TO_RUN=\"-A egress_ip_ipip\" K8ST_REPORT_FILENAME=node-k8s-egress-ip-ipip.xml make --directory=calico-private/${REPO_NAME} k8s-test"
+      ;;
+    egw-vxlan)
+      cmd="K8ST_TO_RUN=\"-A egress_ip_vxlan\" K8ST_REPORT_FILENAME=node-k8s-egress-ip-vxlan.xml make --directory=calico-private/${REPO_NAME} k8s-test"
+￼      ;;
+    *)
+      echo "invalid batch name" && exit 1
+      ;;
+  esac
+
+  VM_NAME="$vm_name" ${remote_exec} $cmd >& "$log_file"
+}
