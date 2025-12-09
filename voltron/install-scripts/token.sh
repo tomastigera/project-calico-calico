135530ba9d4fd08f9bf1426733c2499277b370a6
Jiawei Huang <jiawei@tigera.io>
2025-12-08 19:20:51 -0800

    Remove outdated voltron installation docs and scripts
    Remove outdated and potentially misleading Voltron installation
    docs and scripts that no longer reflects current installation
    practices.

--- voltron/install-scripts/token.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-echo "Retrieving token for ui-user"
-kubectl get secret $(kubectl get secrets --all-namespaces | awk '/ui-user/{print $2}') -n default -o jsonpath="{.data.token}" | base64 --decode
-
-
