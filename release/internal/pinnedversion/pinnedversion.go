2e3d5509a2488685229931f1c27481e6cf600c00
tuti <tuti@tigera.io>
2026-01-19 12:13:42 -0800

    feat(release): include slack response for announced hashrelease (#11676)

--- release/internal/pinnedversion/pinnedversion.go
+++ release/internal/pinnedversion/pinnedversion.go
@@ -368,7 +368,6 @@ func LoadHashrelease(repoRootDir, outputDir, hashreleaseSrcBaseDir string, lates
 		ProductVersion:  pinnedVersion.Title,
 		OperatorVersion: pinnedVersion.TigeraOperator.Version,
 		Source:          filepath.Join(hashreleaseSrcBaseDir, pinnedVersion.Hash),
-		Time:            time.Now(),
 		Latest:          latest,
 	}, nil
 }
