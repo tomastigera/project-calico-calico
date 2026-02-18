c229e252dc8750ea33a588127a39b56e86790392
Jiawei Huang <jiawei@tigera.io>
2026-02-18 15:05:28 -0800

    Fix DPI cache test BeforeSuite misuse
    ```
    Ginkgo detected an issue with your spec structure
    BeforeSuite(func() {
    /go/src/github.com/projectcalico/calico/deep-packet-inspection/pkg/cache/label_selector_cache_test.go:24
      It looks like you are trying to add a [BeforeSuite] node within a container
      node.
      BeforeSuite can only be called at the top level.
      Learn more at:
      http://onsi.github.io/ginkgo/#suite-setup-and-cleanup-beforesuite-and-aftersuite
    ```

--- deep-packet-inspection/pkg/cache/label_selector_cache_test.go
+++ deep-packet-inspection/pkg/cache/label_selector_cache_test.go
@@ -21,7 +21,7 @@ var _ = Describe("SelectorAndLabelCache", func() {
 	var c cache.SelectorAndLabelCache
 	var onMatchStartedCount, onMatchStoppedCount int
 
-	BeforeSuite(func() {
+	BeforeEach(func() {
 		dpiKey1 = model.ResourceKey{Namespace: "test-dpi-ns", Name: "test-dpi-1", Kind: "DeepPacketInspection"}
 		dpiKey2 = model.ResourceKey{Namespace: "test-dpi-ns", Name: "test-dpi-2", Kind: "DeepPacketInspection"}
 		dpiSelector1, err = selector.Parse("label == 'a'")
@@ -38,12 +38,9 @@ var _ = Describe("SelectorAndLabelCache", func() {
 			"label":                       "b",
 			"projectcalico.org/namespace": "default",
 		})
-	})
 
-	BeforeEach(func() {
 		onMatchStartedCount = 0
 		onMatchStoppedCount = 0
-
 	})
 
 	Context("UpdateSelector", func() {
