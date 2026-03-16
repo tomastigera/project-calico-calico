5be05b1b0a7d49b6265ad82ee535929008d3fcab
Nell Jerram <nell@tigera.io>
2026-03-16 19:14:26 +0000

    Merge pull request #12038 from nelljerram/live-migration
    FelixConfiguration and Felix changes for live migration
    
    (cherry picked from commit bd5ef2bcc1ec9eaefa10927f958f4b3fbed4b46a)

--- felix/dataplane/driver.go
+++ felix/dataplane/driver.go
@@ -541,6 +541,13 @@ func StartDataplaneDriver(
 
 			RouteSource: configParams.RouteSource,
 
+			IPv4NormalRoutePriority:   configParams.IPv4NormalRoutePriority,
+			IPv4ElevatedRoutePriority: configParams.IPv4ElevatedRoutePriority,
+			IPv6NormalRoutePriority:   configParams.IPv6NormalRoutePriority,
+			IPv6ElevatedRoutePriority: configParams.IPv6ElevatedRoutePriority,
+
+			LiveMigrationRouteConvergenceTime: configParams.LiveMigrationRouteConvergenceTime,
+
 			KubernetesProvider: configParams.KubernetesProvider(),
 
 			RequireMTUFile: configParams.RequireMTUFile,
